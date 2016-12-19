09733a2dfa725fcc79b4a13f3fd97f3767559414
torz <torz@gmx.com>
2016-12-19 00:29:15 +0000

    fail on not opening on 0

--- interview.py
+++ interview.py
@@ -1,1 +1,27 @@
-import this
+
+
+def isValid(s):
+    parenthesis = []
+    for i in s:
+        if i == '(' or i == ')':
+            parenthesis.append(i)
+
+    if parenthesis[0] == '(':
+        return False
+
+    #for p in parenthesis:
+
+if __name__ == '__main__':
+    isValid('(test)')
+
+
+'''
+create a method that returns true/false
+check for valid parenthesis
+() is valid
+()() is valid
+(()) is valid
+)( is not valid
+(( is not valid
+)) is not valid
+'''
